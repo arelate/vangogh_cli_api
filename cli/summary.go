2125a3245db23c4a712883c1627c4a1c0a631017
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-03-19 20:07:37 -0700

    Moving updates code to REST to avoid a cycle

--- cli/summary.go
+++ cli/summary.go
@@ -3,34 +3,13 @@ package cli
 import (
 	"fmt"
 	"github.com/arelate/gog_integration"
+	v1 "github.com/arelate/vangogh_cli_api/rest/v1"
 	"github.com/arelate/vangogh_local_data"
 	"github.com/boggydigital/nod"
 	"net/url"
 	"sort"
 	"time"
 )
-
-var filterNewProductTypes = map[vangogh_local_data.ProductType]bool{
-	vangogh_local_data.Orders: true,
-	//not all licence-products have associated api-products-v1/api-products-v2,
-	//so in some cases we won't get a meaningful information like a title
-	vangogh_local_data.LicenceProducts: true,
-	//both ApiProductsVx are not interesting since they correspond to store-products or account-products
-	vangogh_local_data.ApiProductsV1: true,
-	vangogh_local_data.ApiProductsV2: true,
-}
-
-var filterUpdatedProductTypes = map[vangogh_local_data.ProductType]bool{
-	//most of the Updates are price changes for a sale, not that interesting for recurring sync
-	vangogh_local_data.StoreProducts: true,
-	// wishlist-products are basically store-products, so see above
-	vangogh_local_data.WishlistProducts: true,
-	//meaningful Updates for account products come from details, not account-products
-	vangogh_local_data.AccountProducts: true,
-	//same as above for those product types
-	vangogh_local_data.ApiProductsV1: true,
-	vangogh_local_data.ApiProductsV2: true,
-}
 
 func SummaryHandler(u *url.URL) error {
 	since, err := vangogh_local_data.SinceFromUrl(u)
@@ -48,7 +27,7 @@ func Summary(mt gog_integration.Media, since int64) error {
 	sa := nod.Begin("key changes since %s:", time.Unix(since, 0).Format("01/02 03:04PM"))
 	defer sa.End()
 
-	updates, err := Updates(mt, since)
+	updates, err := v1.Updates(mt, since)
 	if err != nil {
 		return sa.EndWithError(err)
 	}
@@ -96,43 +75,4 @@ func humanReadable(productTypes map[vangogh_local_data.ProductType]bool) []strin
 	sort.Strings(keys)
 
 	return keys
-}
-
-func categorize(ids []string, cat string, updates map[string]map[string]bool) {
-	for _, id := range ids {
-		if updates[cat] == nil {
-			updates[cat] = make(map[string]bool, 0)
-		}
-		updates[cat][id] = true
-	}
-}
-
-func Updates(mt gog_integration.Media, since int64) (map[string]map[string]bool, error) {
-	updates := make(map[string]map[string]bool, 0)
-
-	for _, pt := range vangogh_local_data.LocalProducts() {
-
-		if filterNewProductTypes[pt] {
-			continue
-		}
-
-		vr, err := vangogh_local_data.NewReader(pt, mt)
-		if err != nil {
-			return updates, err
-		}
-
-		categorize(vr.CreatedAfter(since),
-			fmt.Sprintf("new in %s", pt.HumanReadableString()),
-			updates)
-
-		if filterUpdatedProductTypes[pt] {
-			continue
-		}
-
-		categorize(vr.ModifiedAfter(since, true),
-			fmt.Sprintf("updated in %s", pt.HumanReadableString()),
-			updates)
-	}
-
-	return updates, nil
 }
