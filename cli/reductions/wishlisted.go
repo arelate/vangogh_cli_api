9b6423fbf8650306b8e17d8381ceaa57b3b51874
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-03-25 10:05:09 -0700

    Using WihslistPages instead of _Products for better wishlisted status

--- cli/reductions/wishlisted.go
+++ cli/reductions/wishlisted.go
@@ -4,6 +4,7 @@ import (
 	"github.com/arelate/gog_integration"
 	"github.com/arelate/vangogh_local_data"
 	"github.com/boggydigital/nod"
+	"strconv"
 )
 
 func Wishlisted(mt gog_integration.Media) error {
@@ -16,19 +17,29 @@ func Wishlisted(mt gog_integration.Media) error {
 		return wa.EndWithError(err)
 	}
 
-	vrWishlisted, err := vangogh_local_data.NewReader(vangogh_local_data.WishlistProducts, mt)
+	//using WishlistPage and not WishlistProduct for the remote source of truth
+	vrWishlistPages, err := vangogh_local_data.NewReader(vangogh_local_data.WishlistPage, mt)
 	if err != nil {
 		return wa.EndWithError(err)
 	}
 
 	wishlisted := map[string][]string{}
 
+	//important to set all to false as a starting point to overwrite status
+	//for product no longer wishlisted at the remote source of truth
 	for _, id := range vrStoreProducts.Keys() {
 		wishlisted[id] = []string{"false"}
 	}
 
-	for _, id := range vrWishlisted.Keys() {
-		wishlisted[id] = []string{"true"}
+	for _, page := range vrWishlistPages.Keys() {
+		page, err := vrWishlistPages.ProductsGetter(page)
+		if err != nil {
+			wa.EndWithError(err)
+		}
+		for _, prod := range page.GetProducts() {
+			id := strconv.Itoa(prod.GetId())
+			wishlisted[id] = []string{"true"}
+		}
 	}
 
 	wishlistedRdx, err := vangogh_local_data.ConnectReduxAssets(vangogh_local_data.WishlistedProperty)
