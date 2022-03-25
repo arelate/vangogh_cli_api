2e75c2d0ce34566e0ad920c7a3fba6173a82c9e4
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-03-24 18:15:55 -0700

    Adding limit flag to get first N keys

--- rest/v1/get_keys.go
+++ rest/v1/get_keys.go
@@ -5,6 +5,7 @@ import (
 	"github.com/arelate/vangogh_local_data"
 	"github.com/boggydigital/nod"
 	"net/http"
+	"strconv"
 )
 
 func GetKeys(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +20,10 @@ func GetKeys(w http.ResponseWriter, r *http.Request) {
 
 	pt := vangogh_local_data.ProductTypeFromUrl(r.URL)
 	mt := vangogh_local_data.MediaFromUrl(r.URL)
+	count, err := strconv.Atoi(vangogh_local_data.ValueFromUrl(r.URL, "count"))
+	if err != nil {
+		count = -1
+	}
 
 	sort, desc := sortDescFromUrl(r.URL)
 	if !vangogh_local_data.IsValidProperty(sort) {
@@ -43,6 +48,10 @@ func GetKeys(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
 		return
+	}
+
+	if count > 0 {
+		sortedIds = sortedIds[:count]
 	}
 
 	if err := encode(sortedIds, w, r); err != nil {
