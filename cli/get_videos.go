3ba17bbf826395fdc38830fcb4bacfedffdb02f8
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-04-05 22:29:50 -0700

    Revert "Adding docker specific attempt to locate ffmpeg"
    This reverts commit 612bcf23da16c24ac3474900995d84f83c574d77.

--- cli/get_videos.go
+++ cli/get_videos.go
@@ -8,7 +8,6 @@ import (
 	"github.com/boggydigital/yt_urls"
 	"net/http"
 	"net/url"
-	"os"
 	"os/exec"
 	"path/filepath"
 )
@@ -29,22 +28,12 @@ func GetVideosHandler(u *url.URL) error {
 		vangogh_local_data.FlagFromUrl(u, "missing"))
 }
 
-const dockerFfmpegPath = "/usr/local/sbin/ffmpeg"
-
-func locateFfmpeg() string {
-	if path, err := exec.LookPath("ffmpeg"); err == nil {
-		return path
-	} else if _, err = os.Stat(dockerFfmpegPath); err == nil {
-		return dockerFfmpegPath
-	} else {
-		return ""
-	}
-}
-
 func GetVideos(idSet map[string]bool, ffmpegCmd string, missing bool) error {
 
 	if ffmpegCmd == "" {
-		ffmpegCmd = locateFfmpeg()
+		if path, err := exec.LookPath("ffmpeg"); err == nil {
+			ffmpegCmd = path
+		}
 	}
 
 	gva := nod.NewProgress("getting videos...")
@@ -109,7 +98,7 @@ func GetVideos(idSet map[string]bool, ffmpegCmd string, missing bool) error {
 	return nil
 }
 
-func vgFnDelegate(videoId string, _ *yt_urls.InitialPlayerResponse) string {
+func vgFnDelegate(videoId string, videoPage *yt_urls.InitialPlayerResponse) string {
 	return filepath.Join(
 		vangogh_local_data.AbsDirByVideoId(videoId),
 		videoId+yt_urls.DefaultExt)
