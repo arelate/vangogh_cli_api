bc761264fb834d4306507407a560c2b5383b5d09
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-07-22 20:05:42 -0700

    Implementing IfModifiedSince for data and downloads

--- rest/if_modified_since.go
+++ rest/if_modified_since.go
@@ -1,6 +1,8 @@
 package rest
 
 import (
+	"github.com/arelate/gog_integration"
+	"github.com/arelate/vangogh_local_data"
 	"net/http"
 	"time"
 )
@@ -10,20 +12,70 @@ const (
 	ifModifiedSinceHeader = "If-Modified-Since"
 )
 
+func isNotModified(w http.ResponseWriter, r *http.Request, since int64) bool {
+	utcSince := time.Unix(since, 0).UTC()
+	w.Header().Set(lastModifiedHeader, utcSince.Format(time.RFC1123))
+	if imsh := r.Header.Get(ifModifiedSinceHeader); imsh != "" {
+		if ims, err := time.Parse(time.RFC1123, imsh); err == nil {
+			return utcSince.Unix() <= ims.UTC().Unix()
+		}
+	}
+	return false
+}
+
 func IfReduxModifiedSince(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// redux assets mod time is used to:
 		// 1) set Last-Modified header
 		// 2) check if content was modified since client cache
 		if ramt, err := rxa.ReduxAssetsModTime(); err == nil {
-			utcRamt := time.Unix(ramt, 0).UTC()
-			w.Header().Set(lastModifiedHeader, utcRamt.Format(time.RFC1123))
-			if imsh := r.Header.Get(ifModifiedSinceHeader); imsh != "" {
-				if ims, err := time.Parse(time.RFC1123, imsh); err == nil {
-					if utcRamt.Unix() <= ims.UTC().Unix() {
-						w.WriteHeader(http.StatusNotModified)
-						return
-					}
+			if isNotModified(w, r, ramt) {
+				w.WriteHeader(http.StatusNotModified)
+				return
+			}
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func IfDataModifiedSince(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// data assets mod time is used to:
+		// 1) set Last-Modified header
+		// 2) check if content was modified since client cache
+		//pt := vangogh_local_data.ProductTypeFromUrl(r.URL)
+		pts := vangogh_local_data.ValuesFromUrl(r.URL, "product-type")
+		mt := vangogh_local_data.MediaFromUrl(r.URL)
+		notModified := true
+		for _, pt := range pts {
+			productType := vangogh_local_data.ParseProductType(pt)
+			if vr, err := vangogh_local_data.NewReader(productType, mt); err == nil {
+				if imt, err := vr.IndexCurrentModTime(); err == nil {
+					notModified = notModified && isNotModified(w, r, imt)
+				}
+			}
+		}
+		if notModified {
+			w.WriteHeader(http.StatusNotModified)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func IfDetailsModifiedSince(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// data assets mod time is used to:
+		// 1) set Last-Modified header
+		// 2) check if content was modified since client cache
+		//pt := vangogh_local_data.ProductTypeFromUrl(r.URL)
+		pt := vangogh_local_data.Details
+		mt := gog_integration.Game
+		if vr, err := vangogh_local_data.NewReader(pt, mt); err == nil {
+			if imt, err := vr.IndexCurrentModTime(); err == nil {
+				if isNotModified(w, r, imt) {
+					w.WriteHeader(http.StatusNotModified)
+					return
 				}
 			}
 		}
--- rest/routing.go
+++ rest/routing.go
@@ -6,18 +6,24 @@ import (
 	"net/http"
 )
 
+var (
+	GetOnly   = middleware.GetMethodOnly
+	PatchOnly = middleware.PatchMethodOnly
+	Log       = nod.RequestLog
+)
+
 func HandleFuncs() {
 	patternHandlers := map[string]http.Handler{
-		"/data":      middleware.GetMethodOnly(nod.RequestLog(http.HandlerFunc(GetData))),
-		"/digest":    IfReduxModifiedSince(middleware.GetMethodOnly(nod.RequestLog(http.HandlerFunc(GetDigest)))),
-		"/downloads": middleware.GetMethodOnly(nod.RequestLog(http.HandlerFunc(GetDownloads))),
-		"/has_data":  middleware.GetMethodOnly(nod.RequestLog(http.HandlerFunc(GetHasData))),
-		"/has_redux": IfReduxModifiedSince(middleware.GetMethodOnly(nod.RequestLog(http.HandlerFunc(GetHasRedux)))),
-		"/local_tag": middleware.PatchMethodOnly(nod.RequestLog(http.HandlerFunc(PatchLocalTag))),
-		"/redux":     IfReduxModifiedSince(middleware.GetMethodOnly(nod.RequestLog(http.HandlerFunc(GetRedux)))),
-		"/search":    IfReduxModifiedSince(middleware.GetMethodOnly(nod.RequestLog(http.HandlerFunc(Search)))),
-		"/tag":       middleware.PatchMethodOnly(nod.RequestLog(http.HandlerFunc(PatchTag))),
-		"/wishlist":  nod.RequestLog(http.HandlerFunc(RouteWishlist)),
+		"/data":      IfDataModifiedSince(GetOnly(Log(http.HandlerFunc(GetData)))),
+		"/digest":    IfReduxModifiedSince(GetOnly(Log(http.HandlerFunc(GetDigest)))),
+		"/downloads": IfDetailsModifiedSince(GetOnly(Log(http.HandlerFunc(GetDownloads)))),
+		"/has_data":  IfDataModifiedSince(GetOnly(Log(http.HandlerFunc(GetHasData)))),
+		"/has_redux": IfReduxModifiedSince(GetOnly(Log(http.HandlerFunc(GetHasRedux)))),
+		"/local_tag": PatchOnly(Log(http.HandlerFunc(PatchLocalTag))),
+		"/redux":     IfReduxModifiedSince(GetOnly(Log(http.HandlerFunc(GetRedux)))),
+		"/search":    IfReduxModifiedSince(GetOnly(Log(http.HandlerFunc(Search)))),
+		"/tag":       PatchOnly(Log(http.HandlerFunc(PatchTag))),
+		"/wishlist":  Log(http.HandlerFunc(RouteWishlist)),
 	}
 
 	for p, h := range patternHandlers {
